e0c14d8b03ecd3ab118d2e0d46367da57946ffd7
YuanSuyi <nbysy@qq.com>
2016-06-27 15:52:35 +0800

    pptp add user

--- package/ralink/tmon/src/pptp_mon.c
+++ package/ralink/tmon/src/pptp_mon.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+int find_user(char * ip, char * out_user)
+{
+	char path[100];
+	strcpy(out_user, "unknow");
+	sprintf(path, "/tmp/%s.user", ip);
+	FILE * fp = fopen(path, "r");
+	
+	if (fp == NULL)
+		return -1;
+	fgets(out_user, 100, fp);
+	fclose(fp);
+	return 0;
+}
+
 int find_rx_tx(char * ip, char * out_tx, char * out_rx)
 {
 	char buf[1024];
@@ -29,13 +43,28 @@ int find_rx_tx(char * ip, char * out_tx, char * out_rx)
 			p  = strtok(p, ":");
 			p = strtok(NULL, ":");
 			strcpy(ptp, p);
-		} else if ( (p=strstr(buf, "RX packets:")) != NULL) 
+		} else if ( (p=strstr(buf, "TX bytes:")) != NULL) 
 		{
 			 p = strtok(p+4, " ");
 			 p = strtok(p, ":");
 			 p = strtok(NULL, ":");
+			 strcpy(tx, p);
+
+			 p = strstr(buf, "RX bytes:");
+			 p = strtok(p+4, " "); 
+			 p = strtok(p, ":");
+                         p = strtok(NULL, ":");
 			 strcpy(rx, p);
-		} else if ( (p=strstr(buf, "TX packets:")) != NULL) 
+
+			if (!strcmp(ptp, ip)) 
+                        {
+                                strcpy(out_tx, tx);
+                                strcpy(out_rx, rx);
+                                fclose(fp);
+                                return 0;
+                        }
+
+		} /*else if ( (p=strstr(buf, "TX packets:")) != NULL) 
 		{
 			 p = strtok(p+4, " ");
 			 p = strtok(p, ":");
@@ -48,7 +77,7 @@ int find_rx_tx(char * ip, char * out_tx, char * out_rx)
 				fclose(fp);
 				return 0;
 			}
-		}
+		}*/
 	}
 	fclose(fp);
 	return -1;
@@ -59,7 +88,7 @@ int main()
 	char buf[1024];
 	system("LC_ALL=C ifconfig > /tmp/ifconfig.txt");
 	char * ip = NULL;
-	char  tx[100], rx[100];
+	char  tx[100], rx[100], user[100];
 	FILE * fp = fopen("/tmp/pptp_ip.txt", "r");
 	FILE * fp_out = fopen("/tmp/pptp_stat.txt", "w");
 	if (fp == NULL)
@@ -71,7 +100,8 @@ int main()
 		ip = strtok(buf, " ");
 		if ( find_rx_tx(ip, tx, rx) == 0 )
 		{
-			fprintf(fp_out, "%s %s\n", tx, rx);
+			find_user(ip, user);
+			fprintf(fp_out, "%s %s %s\n", tx, rx, user);
 		}
 	}
 	fclose(fp_out);
