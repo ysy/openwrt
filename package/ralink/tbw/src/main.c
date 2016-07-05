8c1d9634659dc522430042cd87d59a0e34d23108
YuanSuyi <nbysy@qq.com>
2016-07-05 20:31:19 +0800

    tbw app prio

--- package/ralink/tbw/src/main.c
+++ package/ralink/tbw/src/main.c
@@ -24,6 +24,9 @@
 
 #define OPENWRT
  
+ #define BANDS_COUNT  (10)
+char * priomap = "5 6 6 6 5 6 4 4 5 5 5 5 5 5 5 5";
+
 char * get_string_from_file(const char * path,  char * out_buf)
 {
 	char buf[1024];
@@ -58,7 +61,7 @@ static char cmd[1024];
 	} while(0);  
 	
 void decide_iface()
-{
+{  
 	char proto[20];
 	memset(wan_iface, 0, sizeof(wan_iface));
 	memset(lan_iface, 0, sizeof(lan_iface));
@@ -105,12 +108,14 @@ void clear_iptables_mark()
 	fclose(fp);
 }
 
+void set_bw_ip(const char * ip,  int ip_idx, const char * down_limit, const char * up_limit);
+
 void init_iface_qdisc()
 {
 	RUN_CMD("tc qdisc del dev %s root", lan_iface);
 	RUN_CMD("tc qdisc del dev %s root", wan_iface);
-	RUN_CMD("tc qdisc add dev %s root handle 1: htb", lan_iface);
-	RUN_CMD("tc qdisc add dev %s root handle 1: htb", wan_iface);
+	RUN_CMD("tc qdisc add dev %s root handle 1: htb default 2", lan_iface);
+	RUN_CMD("tc qdisc add dev %s root handle 1: htb default 2", wan_iface);
 }
 
 #define DOWN_HANDLE  (0x1000)
@@ -153,47 +158,72 @@ void set_bw(char * iprange, const char * down_limit, const char * up_limit)
 	}
 }
 
+void set_prio(const char * iprange, const char * portrange, const char * prio)
+{
+	RUN_CMD("iptables -t mangle -A PREROUTING -p tcp --dport %s -m iprange --src-range %s \
+			-j MARK --set-mark %s", portrange,  iprange, prio);
+
+	RUN_CMD("iptables -t mangle -A POSTROUTING -p tcp --sport %s -m iprange --dst-range %s \
+			-j MARK --set-mark %s", portrange,  iprange, prio);
+
+	RUN_CMD("iptables -t mangle -A PREROUTING -p udp --dport %s -m iprange --src-range %s \
+			-j MARK --set-mark %s", portrange,  iprange, prio);
+
+	RUN_CMD("iptables -t mangle -A POSTROUTING -p udp --sport %s -m iprange --dst-range %s \
+			-j MARK --set-mark %s", portrange,  iprange, prio);
+}
+
 void set_bw_ip(const char * ip,  int ip_idx, const char * down_limit, const char * up_limit)
 {
 	//idx++;
 	//add iptables mark
 	int idx = ip_idx + 10;
+	if (ip_idx < 0)
+		idx = 2;
+
+	int i = 0;
 	if (strcmp(down_limit, "0") )
-	{
-		//RUN_CMD("iptables -t mangle -A POSTROUTING -m iprange --dst-range %s \
-		//	-j MARK --set-mark %d", iprange, DOWN_HANDLE + idx);
-			
+	{			
 		//limit download, which set tc on lan interface
 		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n", 
 					lan_iface, idx, down_limit);
 
-		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x prio bands 16\n",
-					lan_iface, idx);
+		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x  handle %x: prio bands 10 priomap %s\n",
+					lan_iface, idx, idx, priomap);
 		
-		fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
-				u32 match ip dst %s/32 flowid 1:%x\n", 
-				lan_iface, ip,  idx); 
+		if (ip_idx > 0)
+			fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
+					u32 match ip dst %s/32 flowid 1:%x\n", 
+					lan_iface, ip,  idx); 
+
+		for (i=1; i<=BANDS_COUNT; i++) 
+		{
+			fprintf(fp_cmd, "filter add dev %s parent %x: protocol ip prio 1 \
+				handle 0x%04x fw classid %x:%02x\n", 
+				lan_iface, idx, i, idx, i); 
+		}
 	}
 	
 	if (strcmp(up_limit, "0")) 
 	{
-	/*	RUN_CMD("iptables -t mangle -A PREROUTING -m iprange --src-range %s \
-		 -j MARK --set-mark %d", iprange, UP_HANDLE + idx);
-	*/		
 		//limit upload, which set tc on wan interface
 		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n", 
 					wan_iface, idx, up_limit);
 						
-		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x prio bands 16\n",
-					wan_iface, idx);
+		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x handle %x:  prio bands 10 priomap %s\n",
+					wan_iface, idx, idx, priomap);
 
-		fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
-				u32 match ip src %s/32 flowid 1:%x\n", 
-				wan_iface, ip,  idx); 
+		if (ip_idx > 0 )
+			fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
+					u32 match ip src %s/32 flowid 1:%x\n", 
+					wan_iface, ip,  idx); 
 
-		/*RUN_CMD("tc filter add dev %s parent 1: protocol ip prio 1 \
-				handle 0x%04x fw classid 1:%02x", 
-				wan_iface, UP_HANDLE + idx, idx); */
+		for (i=1; i<=BANDS_COUNT; i++) 
+		{
+			fprintf(fp_cmd, "filter add dev %s parent %x: protocol ip prio 1 \
+				handle 0x%04x fw classid %x:%02x\n", 
+				wan_iface, idx, i, idx, i); 
+		}
 	}
 }
 
@@ -253,9 +283,51 @@ void reload_config()
 			set_bw(iprange, down_limit, up_limit);
 		}
 	}
+
+	set_bw_ip("dummy", -1, "100mbit", "100mbit");
 	fclose(fp_cmd);
 	RUN_CMD("tc -b /tmp/tbw_cmd");
 	fclose(fp);
+
+
+#ifdef OPENWRT
+	fp = fopen("/etc/prio.conf", "r");
+#else
+	fp = fopen("./prio.conf", "r");
+#endif
+
+	if (fp == NULL)
+	{
+		LOG("ERROR: /etc/prio.conf dose not exists");
+		exit(-1);
+	}
+
+	while (fgets(buf,1024, fp))
+	{
+		len = strlen(buf);
+		while(isspace(buf[len-1]) )
+		{
+			buf[len-1] = '\0';
+			len--;
+		}
+		char * iprange = strtok(buf, " ");
+		char * portrange = strtok(NULL,  " ");
+		char * prio = strtok(NULL, " ");
+		char * enabled = strtok(NULL, " ");
+
+
+		if (enabled != NULL && enabled[0] == '0')
+			continue;
+		if (iprange != NULL && portrange != NULL 
+			&& prio != NULL) 
+		{
+			char * p = strstr(portrange, "-");
+			if (p != NULL)
+				*p = ':';
+			set_prio(iprange, portrange, prio);
+		}
+	}
+
 }
 
 int main(int argc,  char * argv[])
