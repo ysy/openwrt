87ac937fafe56db461e1bb9d4783bf7d7d05242a
YuanSuyi <nbysy@qq.com>
2016-08-29 13:52:15 +0800

    tbw: prio setting ok when tbw is off

--- package/ralink/tbw/src/main.c
+++ package/ralink/tbw/src/main.c
@@ -23,7 +23,7 @@
 #include  "log.h"
 
 #define OPENWRT
- 
+
  #define BANDS_COUNT  (10)
 char * priomap = "5 6 6 6 5 6 4 4 5 5 5 5 5 5 5 5";
 
@@ -58,10 +58,10 @@ static char cmd[1024];
 	do{  \
 		sprintf(cmd, fmt, ##args); \
 		system(cmd); \
-	} while(0);  
-	
+	} while(0);
+
 void decide_iface()
-{  
+{
 	char proto[20];
 	memset(wan_iface, 0, sizeof(wan_iface));
 	memset(lan_iface, 0, sizeof(lan_iface));
@@ -89,16 +89,16 @@ void clear_iptables_mark()
 	char buf[1024];
 	system("iptables-save | grep MARK  > /tmp/iptables_mark.txt");
 	FILE * fp = fopen("/tmp/iptables_mark.txt", "r");
-	
-	if (fp == NULL) 
+
+	if (fp == NULL)
 	{
 		LOG("ERROR: error when clear iptables mark");
 		exit(-1);
 	}
-	
-	while(fgets(buf, 1024, fp)) 
+
+	while(fgets(buf, 1024, fp))
 	{
-		if (buf[0] == '-' && buf[1] == 'A') 
+		if (buf[0] == '-' && buf[1] == 'A')
 		{
 			buf[1] = 'D';
 			RUN_CMD("iptables -t mangle %s", buf);
@@ -131,7 +131,7 @@ void set_bw(char * iprange, const char * down_limit, const char * up_limit)
 	int start_idx, end_idx;
 	char * p = strtok(iprange, "-");
 	if (p == NULL)
-		return;	
+		return;
 	strcpy(ip_start, p);
 	p = strtok(NULL, "-");
 	if (p == NULL)
@@ -151,7 +151,7 @@ void set_bw(char * iprange, const char * down_limit, const char * up_limit)
 	int i = 0;
 
 	char ip[30];
-	for (i=start_idx; i<=end_idx; i++) 
+	for (i=start_idx; i<=end_idx; i++)
 	{
 		sprintf(ip, "%s.%d", ip_sub, i);
 		set_bw_ip(ip, i, down_limit, up_limit);
@@ -183,113 +183,105 @@ void set_bw_ip(const char * ip,  int ip_idx, const char * down_limit, const char
 
 	int i = 0;
 	if (strcmp(down_limit, "0") )
-	{			
+	{
 		//limit download, which set tc on lan interface
-		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n", 
+		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n",
 					lan_iface, idx, down_limit);
 
 		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x  handle %x: prio bands 10 priomap %s\n",
 					lan_iface, idx, idx, priomap);
-		
+
 		if (ip_idx > 0)
 			fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
-					u32 match ip dst %s/32 flowid 1:%x\n", 
-					lan_iface, ip,  idx); 
+					u32 match ip dst %s/32 flowid 1:%x\n",
+					lan_iface, ip,  idx);
 
-		for (i=1; i<=BANDS_COUNT; i++) 
+		for (i=1; i<=BANDS_COUNT; i++)
 		{
 			fprintf(fp_cmd, "filter add dev %s parent %x: protocol ip prio 1 \
-				handle 0x%04x fw classid %x:%02x\n", 
-				lan_iface, idx, i, idx, i); 
+				handle 0x%04x fw classid %x:%02x\n",
+				lan_iface, idx, i, idx, i);
 		}
 	}
-	
-	if (strcmp(up_limit, "0")) 
+
+	if (strcmp(up_limit, "0"))
 	{
 		//limit upload, which set tc on wan interface
-		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n", 
+		fprintf(fp_cmd, "class add dev %s parent 1: classid 1:%x htb rate %s quantum 1500\n",
 					wan_iface, idx, up_limit);
-						
+
 		fprintf(fp_cmd,"qdisc add dev %s parent 1:%x handle %x:  prio bands 10 priomap %s\n",
 					wan_iface, idx, idx, priomap);
 
 		if (ip_idx > 0 )
 			fprintf(fp_cmd,"filter add dev %s parent 1: protocol ip prio 1 \
-					u32 match ip src %s/32 flowid 1:%x\n", 
-					wan_iface, ip,  idx); 
+					u32 match ip src %s/32 flowid 1:%x\n",
+					wan_iface, ip,  idx);
 
-		for (i=1; i<=BANDS_COUNT; i++) 
+		for (i=1; i<=BANDS_COUNT; i++)
 		{
 			fprintf(fp_cmd, "filter add dev %s parent %x: protocol ip prio 1 \
-				handle 0x%04x fw classid %x:%02x\n", 
-				wan_iface, idx, i, idx, i); 
+				handle 0x%04x fw classid %x:%02x\n",
+				wan_iface, idx, i, idx, i);
 		}
 	}
 }
 
-
-void reload_config()
+void tbw_config()
 {
 	char buf[1024];
-	int len =0; 
+	int len =0;
 	FILE * fp = NULL;
-	
-	decide_iface();
-	clear_iptables_mark();
-	init_iface_qdisc();
-#ifdef OPENWRT
+
+	#ifdef OPENWRT
 	fp = fopen("/etc/tbw.conf", "r");
 #else
 	fp = fopen("./tbw.conf", "r");
 #endif
-
-	if (fp == NULL)
-	{
-		LOG("ERROR: /etc/tbw.conf dose not exists");
-		exit(-1);
-	}
-	
-	memset(buf, 0, sizeof(buf));
-	if ( fgets(buf, 1024, fp)  == NULL )
-	{
-		LOG("ERROR: /etc/tbw.conf is empty");
-		return;
-	}
-	
-	if (!strncmp(buf, "off", 3))
-	{
-		LOG("Bandwith control is disabled");
-		return;
-	}
-	
 	fp_cmd = fopen("/tmp/tbw_cmd", "w");
-	while (fgets(buf,1024, fp))
+	memset(buf, 0, sizeof(buf));
+	if ( fp && fgets(buf, 1024, fp) && !strncmp(buf, "on", 2))
 	{
-		len = strlen(buf);
-		while(isspace(buf[len-1]) )
+		while (fgets(buf,1024, fp))
 		{
-			buf[len-1] = '\0';
-			len--;
-		}
-		char * iprange = strtok(buf, " ");
-		char * down_limit = strtok(NULL, " ");
-		char * up_limit = strtok(NULL, " ");
-		char * enabled = strtok(NULL, " ");
-		if (enabled != NULL && enabled[0] == '0')
-			continue;
-		if (iprange != NULL && down_limit != NULL 
-			&& up_limit != NULL) 
-		{
-			set_bw(iprange, down_limit, up_limit);
+			len = strlen(buf);
+			while(isspace(buf[len-1]) )
+			{
+				buf[len-1] = '\0';
+				len--;
+			}
+			char * iprange = strtok(buf, " ");
+			char * down_limit = strtok(NULL, " ");
+			char * up_limit = strtok(NULL, " ");
+			char * enabled = strtok(NULL, " ");
+			if (enabled != NULL && enabled[0] == '0')
+				continue;
+			if (iprange != NULL && down_limit != NULL
+				&& up_limit != NULL)
+			{
+				set_bw(iprange, down_limit, up_limit);
+			}
 		}
 	}
 
 	set_bw_ip("dummy", -1, "100mbit", "100mbit");
 	fclose(fp_cmd);
 	RUN_CMD("tc -b /tmp/tbw_cmd");
-	fclose(fp);
+	if (fp)
+		fclose(fp);
+}
 
+void reload_config()
+{
+	char buf[1024];
+	int len =0;
+	FILE * fp = NULL;
 
+	decide_iface();
+	clear_iptables_mark();
+	init_iface_qdisc();
+
+	tbw_config();
 #ifdef OPENWRT
 	fp = fopen("/etc/prio.conf", "r");
 #else
@@ -318,8 +310,8 @@ void reload_config()
 
 		if (enabled != NULL && enabled[0] == '0')
 			continue;
-		if (iprange != NULL && portrange != NULL 
-			&& prio != NULL) 
+		if (iprange != NULL && portrange != NULL
+			&& prio != NULL)
 		{
 			char * p = strstr(portrange, "-");
 			if (p != NULL)
@@ -331,7 +323,7 @@ void reload_config()
 }
 
 int main(int argc,  char * argv[])
-{  
+{
 	INIT_LOG("TBW");
 	LOG("Bandwith control program");
 	good_checker();
